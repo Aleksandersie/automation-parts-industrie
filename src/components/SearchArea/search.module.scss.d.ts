a89a73a8f9dd1c1f9c25a087eae3476052ca9cf0
Aleksandersie <aleksander.sie@gmail.com>
2023-01-15 00:40:20 +0300

    init

---
+++ src/components/SearchArea/search.module.scss.d.ts
@@ -0,0 +1,5 @@
+export const body: string
+export const btn: string
+export const input: string
+export const searchBlock: string
+export const searchRes: string
---
+++ src/components/TopStrip/style.module.scss.d.ts
@@ -0,0 +1,5 @@
+export const firstLetter: string
+export const login: string
+export const logo: string
+export const stripContainer: string
+export const topStrip: string
