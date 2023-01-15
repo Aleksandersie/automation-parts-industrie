99dd65dda5097a27fad91ade4fc6539d218a4d6c
Aleksandersie <aleksander.sie@gmail.com>
2023-01-15 23:33:31 +0300

    cards fix

---
+++ src/components/SmallCard/smallCard.module.scss.d.ts
@@ -0,0 +1,6 @@
+export const bottomText: string
+export const buyBtn: string
+export const cardBody: string
+export const itemImg: string
+export const itemName: string
+export const priceValue: string
